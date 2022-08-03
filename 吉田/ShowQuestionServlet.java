6709ff1075e6a887b4ce78c162fff9b4a5d13d7d
YoshidaNobuya <103481201+YoshidaNobuya@users.noreply.github.com>
2022-08-03 09:16:35 +0900

    完成版v2
    2問以降に問題設定処理をスキップする処理を追加

--- 吉田/ShowQuestionServlet.java
+++ 吉田/ShowQuestionServlet.java
@@ -19,26 +19,36 @@ import model.QuestionLogic;
 public class ShowQuestionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// カテゴリ番号をリクエストパラメータから取得
-		int cateNum=Integer.parseInt(request.getParameter("cate"));
+		int cateNum = Integer.parseInt(request.getParameter("cate")); // カテゴリ番号
 
-		//QuestionDAOクラスに処理を依頼
-		QuestionDAO qDAO=new QuestionDAO();
-		ArrayList<Question> questionList=qDAO.findAll(cateNum);
+		if (cateNum > 0 && cateNum < 8) {	//１問目開始時は問題を設定する処理を行う
+			// QuestionDAOクラスに処理を依頼
+			QuestionDAO qDAO = new QuestionDAO();
+			ArrayList<Question> questionList = qDAO.findAll(cateNum);
 
-		//QuestionLogicクラスに処理を依頼
-		Question[] q10=new Question[10];
-		q10=QuestionLogic.setQuestion(questionList);
+			// QuestionLogicクラスに処理を依頼
+			Question[] q10 = new Question[10];
+			q10 = QuestionLogic.setQuestion(questionList);
 
-		//セッションスコープにインスタンスを保存
-		HttpSession session=request.getSession();
-		int count=0;
-		session.setAttribute("count", count);
-		session.setAttribute("q10", q10);
+			// セッションスコープにインスタンスを保存
+			int count = 0; // 問題数カウント
+			HttpSession session = request.getSession();
+			session.setAttribute("count", count);
+			session.setAttribute("q10", q10);
+		} else if (cateNum == 0) {
+			// 2問目以降は上記処理をスキップする
+		} else {
+			// 不正な値を受け取ったらTOPページに戻る
+			HttpSession session = request.getSession();
+			session.invalidate();
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/top.jsp");
+			dispatcher.forward(request, response);
+		}
 
-		//showQuestion.jspにフォワード
+		// showQuestion.jspにフォワード
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/showQuestion.jsp");
 		dispatcher.forward(request, response);
 
